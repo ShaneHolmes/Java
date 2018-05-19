ee8304ad3284ec3d51346ceb7865bd3a02274645
吴多贤 <474638272@qq.com>
2018-05-19 13:20:17 +0800

    Create Course.java

---
+++ Projects/JavaAssignments/zyd/hw1/crs/Course.java
@@ -0,0 +1,34 @@
+/*
+ *@author ShaneHolmes
+ * date:2018.5.7
+ *@Right Copy.All right reserved.
+**/
+package zyd.hw1.crs;
+
+public class Course {
+	private String cid;
+	private String name;
+	private Book[] books;
+
+	public Course(String cid, String name, Book[] books) {
+		this.cid = cid;
+		this.name = name;
+		this.books = books;
+	}
+
+	public Book[] getBooks() {
+		return this.books;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String bookslist(){
+		String list = "";
+		for(Book book : books){
+			list += "," + book.getName();
+		}
+		return list.replaceFirst(",", "");
+	}
+}
