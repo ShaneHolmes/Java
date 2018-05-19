b1789d38c4f42994bd3d9a081825daa01fc95ace
吴多贤 <474638272@qq.com>
2018-05-19 13:19:34 +0800

    Create Book.java

---
+++ Projects/JavaAssignments/zyd/hw1/crs/Book.java
@@ -0,0 +1,21 @@
+/*
+ *@author ShaneHolmes
+ * date:2018.5.7
+ *@Right Copy.All right reserved.
+**/
+package zyd.hw1.crs;
+public class Book {
+	private String name;
+
+	public Book(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
